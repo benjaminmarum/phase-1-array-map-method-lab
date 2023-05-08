d33c8a2b98d206af3a2d96f5f426e243464e50c5
Ben Marum <ben.j.marum@gmail.com>
2023-05-08 16:49:08 -0600

    completed lab

--- index.js
+++ index.js
@@ -11,6 +11,53 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// const titleCased = () => {
+//   return tutorials
+// }
+
+//========================================================================================================================>
+//Examples
+//Map with declared function 
+function studentRollCall(student) {
+  return student + " the skier";
 }
+
+const skiSchool = ["aki", "guadalupe", "lei", "aalam"];
+const rollCall = skiSchool.map(studentRollCall);
+
+//Map with function expression
+const skiSchool2 = ["aki", "guadalupe", "lei", "aalam"];
+const rollCall2 = skiSchool2.map(function (student) {
+  return student + " the skier";
+});
+
+//Map with arrow function
+const skiSchool3 = ["aki", "guadalupe", "lei", "aalam"];
+const rollCall3 = skiSchool3.map((student) => student + " the skier");
+
+//Map with data structures
+const robots = [
+  { name: "Johnny 5", modes: 5, isActivated: false },
+  { name: "C3PO", modes: 3, isActivated: false },
+  { name: "Sonny", modes: 2.5, isActivated: false },
+  { name: "Baymax", modes: 1.5, isActivated: false },
+];
+
+const activatedRobots = robots.map((robot) => {
+  return Object.assign({}, robot, {
+    modes: robot.modes * 2,
+    isActivated: true,
+  });
+});
+
+
+//========================================================================================================================>
+
+function titleCased() {
+  let wordArray = tutorials.map((string) => {
+    const stringArray = string.split(" ")
+    const capWordsArray = stringArray.map(word => word[0].toUpperCase() + word.slice(1))
+    return capWordsArray.join(" ");
+  })
+  return wordArray
+};
